cc91dc24c08dade60f6f507a903c2f59249c9ac5
Seojin Lee <jeje0329@naver.com>
2025-08-05 18:22:20 +0900

    init

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-
 const path = require('path');
+
+
 //✅ public 폴더 안에 있는 정적 파일 서빙
-app.use(express.static(path.join(dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //✅ 루트 경로에 들어오면 test.html 서빙
 app.get('/', (req, res) => {
