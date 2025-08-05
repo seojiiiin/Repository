51c200a616097dc01df1fb7c7ac0b837953048bb
Seojin Lee <jeje0329@naver.com>
2025-08-05 17:07:16 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// index.js
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// const path = require('path');
+// // ✅ public 폴더 안에 있는 정적 파일 서빙
+// app.use(express.static(path.join(dirname, 'public')));
+
+// // ✅ 루트 경로에 들어오면 test.html 서빙
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(dirname, 'public', 'test.html'));
+// });
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello from Render with Node.js!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
